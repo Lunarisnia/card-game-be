c4df115d7111768fbee3c683b94aacfaf0bf5882
Loona <r.arswendo.r@gmail.com>
2022-12-12 15:11:33 +0700

    feat(Card): refactored data handling

--- src/libs/Card.py
+++ src/libs/Card.py
@@ -9,6 +9,19 @@ class Gender(Enum):
     MALE = "Male"
     FEMALE = "Female"
 
+class CardModel(PyMongoUtility):
+    def __init__(self, document=None) -> None:
+        super().__init__(document)
+        self.id = document['_id']
+        self.type = document['type']
+        self.description = document['description']
+
+
+class GraveyardModel(PyMongoUtility):
+    def __init__(self, document=None) -> None:
+        super().__init__(document)
+        self.id = document['_id']
+        self.card_id = document['card_id']
 
 class Card(BaseModel):
     def __init__(self) -> None:
@@ -20,8 +33,8 @@ class Card(BaseModel):
         if len(cards) < 1: return None
         shuffle(cards)
         drawn_card = cards.pop()
-        self.put_card_away(drawn_card['_id'])
-        return PyMongoUtility(drawn_card).to_json()
+        self.put_card_away(drawn_card.id)
+        return drawn_card.to_json()
 
     def put_card_away(self, card_id):
         self.col.get_collection('graveyard').insert_one({"card_id": card_id})
@@ -29,13 +42,14 @@ class Card(BaseModel):
     def fetch_cards(self, gender=[Gender.MALE]):
         graveyard = self.fetch_graveyard()
         cards = self.col.get_collection('cards').find(
-            {"_id": {"$nin": [body['card_id'] for body in graveyard]}, "gender": {"$in": [g.value for g in gender]}}).to_python()
-        return cards
+            {"_id": {"$nin": [body.card_id for body in graveyard]}, "gender": {"$in": [g.value for g in gender]}}).to_python()
+        return [CardModel(card) for card in cards]
 
     def fetch_graveyard(self):
-        return self.col.get_collection('graveyard').find().to_python()
+        cards = self.col.get_collection('graveyard').find().to_python()
+        return [GraveyardModel(card) for card in cards]
 
     def reset_graveyard(self):
         graveyard = self.fetch_graveyard()
-        self.col.delete_many({"_id": {"$in": [body['_id'] for body in graveyard]}})
+        self.col.delete_many({"_id": {"$in": [body.id for body in graveyard]}})
         return {"message": "Graveyard is reset!"}
