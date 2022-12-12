488fd055be0ae9d3337131f9c1d2e08985ab9a28
Loona <r.arswendo.r@gmail.com>
2022-12-12 15:33:50 +0700

    feat(Cards): added dummy migration for the cards

---
+++ src/migration/CardsMigration.py
@@ -0,0 +1,55 @@
+lipsum = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper libero mattis commodo mollis. Curabitur consequat lacus quis neque iaculis viverra. Phasellus hendrerit feugiat mi. Nunc eget convallis ex. Fusce placerat, velit vitae feugiat tristique, turpis orci pulvinar turpis, ac pulvinar eros nisl eget tortor. Vestibulum velit eros, vehicula quis posuere nec, sagittis eget magna. Ut et commodo metus. Maecenas sed velit convallis, facilisis tortor a, finibus nibh. Aliquam erat volutpat. Pellentesque vestibulum mollis neque ut pharetra. Quisque egestas massa id tellus semper placerat.
+
+Sed sodales elit in maximus molestie. Aenean vulputate elit commodo orci fermentum egestas. Mauris id mattis odio, nec ultrices eros. Integer lacinia, justo id rutrum hendrerit, ante odio mattis nibh, quis semper erat eros dapibus augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc a sapien vitae justo finibus rhoncus id scelerisque ante. Nunc placerat consequat tellus, ac dictum dui gravida malesuada.
+
+Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In at fermentum nulla, non placerat nunc. Sed nec est pharetra, tincidunt velit interdum, facilisis elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus eget rutrum lacus. Aliquam eget justo eu diam consequat accumsan. Nulla egestas orci eros, vel auctor diam rutrum eu. Donec nec felis a lectus auctor hendrerit nec nec ipsum. Morbi luctus magna sem, vel posuere justo auctor eu. Suspendisse et massa in leo elementum porta. Morbi accumsan mi quis fringilla venenatis.
+
+Aliquam interdum viverra elit, sed blandit tortor elementum at. Nunc tincidunt tempor dolor id tristique. Phasellus a quam pretium, dignissim nulla quis, mollis velit. Phasellus sagittis ligula id tellus commodo iaculis. Praesent aliquam euismod fringilla. Vivamus dignissim pharetra elit eu vehicula. Phasellus nec euismod risus.
+
+Curabitur in lorem erat. In et mauris nulla. Curabitur imperdiet scelerisque magna a tincidunt. Donec ullamcorper, ipsum eget fringilla molestie, sapien massa elementum quam, sit amet vehicula magna ipsum in felis. Nulla efficitur gravida velit quis lacinia. Donec dignissim eleifend pellentesque. In tristique arcu eu pellentesque blandit. In hac habitasse platea dictumst. Phasellus at turpis accumsan, rhoncus eros et, pharetra odio. Aliquam erat volutpat. Quisque facilisis turpis eros, at mollis ligula varius et. Mauris facilisis eu magna id accumsan. Nam in fermentum nibh. Quisque aliquet, dolor in convallis elementum, velit tortor sollicitudin ex, eget blandit neque justo sit amet orci. Aliquam varius ultrices vestibulum.
+
+Suspendisse lorem felis, molestie eu scelerisque eu, elementum quis turpis. Fusce sed lectus lectus. Praesent posuere sed ex vel interdum. Vestibulum varius imperdiet massa, non fringilla ante tempor scelerisque. Vivamus quis dignissim metus, sed maximus tortor. Proin lobortis feugiat nulla, ac semper dui. Maecenas semper, sapien ut gravida hendrerit, sem neque molestie risus, iaculis porttitor sem ante sit amet enim. Pellentesque in augue vel nulla rutrum tincidunt. Aliquam sit amet iaculis urna. Vivamus malesuada vehicula enim, quis dictum augue. Vivamus et posuere urna, a elementum ligula. Aenean non massa non nunc maximus convallis.
+
+Sed ultrices mi quis nisl mattis faucibus. Pellentesque at mi lorem. Maecenas nibh felis, viverra sit amet blandit a, porta ut dolor. Morbi purus lorem, scelerisque ut commodo ac, gravida a est. Vestibulum viverra urna sit amet purus aliquam convallis. Curabitur eu risus lobortis, fringilla erat quis, vulputate quam. Vivamus euismod, leo eget sodales malesuada, urna odio consectetur mi, sed accumsan metus urna quis massa. Aenean ut turpis in nunc consectetur congue in ut ex. Phasellus sagittis mauris non est ullamcorper volutpat.
+
+Mauris ut iaculis est, imperdiet tristique sem. Cras quis ante bibendum, auctor dolor quis, tincidunt metus. Maecenas id tellus in diam vehicula scelerisque. Duis mattis justo eget semper interdum. Praesent sed quam dui. Phasellus et orci a nisi rhoncus gravida ac non mauris. Donec in elementum augue. Sed mollis sollicitudin est et pretium. Ut metus ligula, vestibulum et sem eu, finibus varius ante.
+
+Integer vel pellentesque eros, in porttitor elit. Proin eget velit eget ligula faucibus suscipit. Nam mollis, sapien aliquam sodales bibendum, velit arcu iaculis ligula, sed elementum arcu nulla eu est. Nam nec libero vitae nunc sollicitudin porta. Vestibulum eu lectus cursus, porta lorem id, faucibus velit. Curabitur dictum elit sit amet tincidunt malesuada. Fusce suscipit quam dignissim mattis gravida. Etiam sit amet nulla commodo, vulputate metus nec, bibendum quam. Nullam pharetra nunc elit, at sodales lorem venenatis nec. Nunc sed dui at nisl placerat maximus ut vitae mi. Morbi ut erat mattis, varius ante aliquet, placerat neque. Nunc ante sapien, aliquet eu erat ac, laoreet dapibus arcu. Morbi varius velit scelerisque tortor aliquet iaculis.
+
+Ut imperdiet dui ullamcorper nunc semper, eu mattis arcu congue. Duis augue purus, dapibus nec fringilla et, tincidunt sed turpis. Suspendisse et placerat orci. Curabitur rutrum turpis id faucibus pellentesque. Sed eu ultrices velit, at feugiat felis. Morbi rutrum nibh at blandit cursus. Nulla sed scelerisque diam, sed scelerisque augue. Morbi fermentum enim magna, et gravida enim hendrerit nec. Sed id ultricies nunc. In ultricies risus ut maximus sagittis. Etiam auctor dui augue, vitae sodales enim venenatis ac. Aliquam pulvinar efficitur ante ac ornare. Maecenas at tortor lacinia, cursus ante a, malesuada urna. Etiam id lacus risus. Integer porta egestas elementum.
+
+Vestibulum neque elit, rutrum ut velit ac, sodales lobortis lorem. Fusce auctor velit sit amet justo iaculis, id lacinia nunc pharetra. Nulla suscipit semper mi, vitae pretium leo porta et. In quam nulla, ornare eu rutrum ac, imperdiet in libero. Aliquam erat volutpat. Curabitur mattis condimentum bibendum. Mauris ornare elit quam, quis placerat lacus aliquam a.
+
+Fusce pulvinar nulla a magna rhoncus laoreet. Duis interdum sapien at sagittis rhoncus. Nunc blandit mauris consequat mollis dignissim. Cras elementum ullamcorper diam, nec ultricies quam ultrices a. Morbi ac arcu nec magna bibendum ultricies. Mauris tincidunt, orci nec scelerisque dignissim, nunc erat laoreet purus, id finibus mi ligula ut dui. Fusce a nisl eget ipsum maximus facilisis. Vestibulum blandit id quam in rhoncus. Phasellus tempus auctor sapien in consectetur.
+
+Suspendisse potenti. Phasellus tristique et sapien id dignissim. Integer hendrerit imperdiet odio, eu rhoncus turpis scelerisque sed. Quisque elementum ornare feugiat. Donec tristique, ex eu accumsan fermentum, orci turpis vestibulum odio, quis lacinia justo lectus non lectus. Ut erat ipsum, ultricies quis tempus dictum, scelerisque nec tellus. Duis scelerisque nisl sit amet ex consequat consectetur. Nullam commodo enim non tellus molestie auctor. In nec risus enim. Aliquam vehicula mauris nisi, et sollicitudin nisl pharetra et.
+
+Sed iaculis consectetur eleifend. Aliquam vulputate nulla magna, eu vulputate augue maximus eu. Duis a quam vel nibh molestie mattis. In hac habitasse platea dictumst. Cras et justo vehicula, egestas dui non, iaculis odio. Duis consequat orci maximus lacus commodo tincidunt. Praesent sit amet sapien orci. Donec semper ipsum mauris.
+
+In ante justo, pretium ut nisi et, lobortis tempor tortor. In sed tempus neque, congue commodo nibh. Nullam eget maximus ligula. Vivamus metus ante, ullamcorper sit amet blandit eu, pellentesque ut odio. Cras volutpat laoreet mi, vitae fermentum nisl pellentesque vel. Duis feugiat ante eu magna elementum ornare. Pellentesque eleifend eget sapien vel vulputate. Maecenas risus sapien, commodo a ultrices euismod, semper non urna. Nulla quis posuere mauris. Fusce eu commodo risus. Aliquam venenatis velit leo, rhoncus ultrices ipsum facilisis id. Integer vel rhoncus purus. Pellentesque porta, enim quis aliquam posuere, dui libero interdum ex, et interdum enim diam ac dui. Donec volutpat interdum turpis, eget interdum tellus iaculis id.
+
+Duis urna justo, placerat quis nisi elementum, interdum fringilla orci. Nulla scelerisque lectus ut diam viverra, et euismod nisl hendrerit. Sed fringilla, lacus a auctor molestie, ligula tellus aliquet orci, sit amet gravida odio risus sed arcu. Phasellus condimentum massa ac lorem suscipit, eu tempor ligula ornare. Curabitur sem sem, rhoncus sit amet tempor quis, varius ut lorem. Nullam libero odio, vestibulum vel egestas non, condimentum in odio. Donec non lorem mattis, suscipit justo id, maximus mauris. Suspendisse quis diam sapien. Aliquam faucibus eget nisi ut sollicitudin. In hac habitasse platea dictumst.
+
+Suspendisse vel dolor et nisl suscipit ultrices. Pellentesque a velit vehicula, cursus ex nec, sollicitudin libero. Integer interdum ipsum tincidunt, pharetra erat nec, feugiat ex. Nunc leo nibh, auctor ac lacus vel, sollicitudin fermentum est. Nunc ut libero aliquet purus semper vulputate a ac elit. Sed cursus, purus at pharetra finibus, velit purus luctus nulla, ac fringilla ipsum ipsum eu velit. Praesent iaculis augue quis urna tempus pharetra. Nullam consectetur auctor congue. Donec dapibus elit eu auctor placerat. In vehicula est vel nisl dictum, sit amet tincidunt purus malesuada. Quisque faucibus enim mattis tellus vehicula, elementum auctor est cursus.
+
+Cras aliquam ex vehicula, posuere nunc vel, dapibus dui. Proin hendrerit at est tristique consectetur. Morbi at enim metus. Curabitur a nibh vel ante varius egestas semper sed elit. Curabitur varius lacus libero, non tincidunt lacus suscipit laoreet. Sed at scelerisque odio. Maecenas varius tellus nunc, nec volutpat ipsum imperdiet ut. Nam fermentum quam tortor, in congue neque suscipit sit amet. Etiam imperdiet, nisl sit amet varius blandit, odio mauris dignissim dolor, ac elementum nibh lacus in dolor. Morbi rhoncus libero in aliquam eleifend. Ut vel ipsum nec lacus congue imperdiet id eget magna. Vivamus urna ligula, iaculis vestibulum ipsum."""
+
+from pymongo import MongoClient
+import random
+import os
+import dotenv
+dotenv.load_dotenv()
+
+connection = MongoClient(os.environ.get('CONNECTION_STRING'))[os.environ.get('DATABASE_NAME')]
+
+dummy_cards = [
+    {
+        "type": "question" if random.randint(0, 1) == 0 else "activities",
+        "gender": ["Male", "Female"],
+        "description": dummy
+    } for dummy in lipsum.split(". ")
+]
+
+connection['cards'].insert_many(dummy_cards)
+
+print("Migrating Cards Complete!")
